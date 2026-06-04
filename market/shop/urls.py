cefec93b27586714c1f87adbf1381375a7b1731a
bakhtyari <niloobakhtiari92@gmail.com>
2026-06-04 23:49:49 +0330

    chenge template

--- market/shop/urls.py
+++ market/shop/urls.py
@@ -2,13 +2,14 @@
 # from django.urls import path , include
 # from . import views
 from django.urls import path
-from . import views
+from .views import product_list , about
 
 # urlpatterns = [
     
 #     path('' , include('shop.urls')),
 #  ]
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('', product_list , name="home"),
+    # path('', product_detail),
+    path('/about/' , about , name="about"),
 ]
